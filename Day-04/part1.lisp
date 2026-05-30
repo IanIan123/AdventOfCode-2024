61752d62cc432daed5a72c726062922f52b38462
Ian <ian.warburton3000@gmail.com>
2026-05-30 23:25:31 +0100

    Refactor

--- Day-04/part1.lisp
+++ Day-04/part1.lisp
@@ -1,5 +1,4 @@
 (ql:quickload "split-sequence")
-
 (use-package :split-sequence)
 
 (defun read-file (filename)
@@ -10,23 +9,47 @@
 (defun string-to-string-list (str)
    (mapcar #'string (coerce str 'list)))
 
-(defun list-of-lists-to-2d-array (list-of-lists)
-  (let* ((rows (length list-of-lists))
-         (cols (length (first list-of-lists)))
-         (array (make-array (list rows cols) :initial-element "")))
-    (dotimes (i rows)
-      (dotimes (j cols)
-        (setf (aref array i j) (nth j (nth i list-of-lists)))))
-    array))
+(defmacro define-rotate-grid (name new-x-expr new-y-expr)
+  `(defun ,name (grid)
+     (let* ((rows (length grid))
+            (cols (length (first grid)))
+            (new-size (+ rows cols -1))
+            (rotated-grid (create-empty-grid new-size)))
+       (dotimes (i rows)
+         (dotimes (j cols)
+           (let ((new-x ,new-x-expr)
+                 (new-y ,new-y-expr))
+             ;; Ensure the indices are within bounds
+             (when (and (>= new-x 0) (< new-x new-size)
+                        (>= new-y 0) (< new-y new-size))
+               (setf (aref rotated-grid new-x new-y) (nth j (nth i grid)))))))
+       (flatten (concatenate-strings2 (mapcar (lambda (row) (remove nil row)) (2d-array-to-list rotated-grid)))))))
 
-(defun lookup-char (x y grid)
-    (nth y (nth x  grid)))
+(define-rotate-grid rotate-grid-45
+  (+ i j)
+  (- (+ cols i) j))
 
-(defun transpose-grid (grid)
-  (apply #'mapcar (lambda (&rest rows) rows) grid))
-  
-(defun concatenate-strings (string-list)
-  (apply #'concatenate 'string string-list))
+(define-rotate-grid rotate-grid-45-reverse
+  (+ j (- rows i 1))
+  (+ i j))
+
+(defun create-empty-grid (size)
+  (make-array (list size size) :initial-element nil))
+
+(defun 2d-array-to-list (array)
+  (loop for i below (array-dimension array 0)
+    collect (loop for j below (array-dimension array 1)
+      collect (aref array i j))))
+
+(defun concatenate-strings2 (nested-lists)
+  (mapcar (lambda (inner-list)
+    (list (apply #'concatenate 'string inner-list)))
+      nested-lists))
+
+(defun flatten (nested-list)
+  (if (atom nested-list)
+      (list nested-list)
+      (apply #'append (mapcar #'flatten nested-list))))
 
 (defun copy-and-flatten-grid (grid)
   (let* ((horizontal-copies (copy-seq (mapcar #'concatenate-strings grid)))
@@ -34,65 +57,17 @@
          (vertical-copies (copy-seq (mapcar #'concatenate-strings transposed-grid))))
    (append horizontal-copies vertical-copies)))
 
-(defun create-empty-grid (size)
-  (make-array (list size size) :initial-element nil))
+(defun transpose-grid (grid)
+  (apply #'mapcar (lambda (&rest rows) rows) grid))
 
-(defun place-in-rotated-grid (grid x y element)
-  (setf (aref grid x y) element))
-
-(defun 2d-array-to-list (array)
-  (loop for i below (array-dimension array 0)
-        collect (loop for j below (array-dimension array 1)
-                      collect (aref array i j))))
-
-(defun concatenate-strings2 (nested-lists)
-  (mapcar (lambda (inner-list)
-            (list (apply #'concatenate 'string inner-list)))
-          nested-lists))
-
-(defun concatenate-inner-lists (nested-lists)
-  (mapcar (lambda (inner-lists)
-            (mapcar (lambda (strings)
-                      (apply #'concatenate 'string (map #'identity strings)))
-                    inner-lists))
-          nested-lists))
-
-(defun rotate-grid-45 (grid)
-  (let* ((rows (length grid))
-         (cols (length (first grid)))
-         (new-size (+ rows cols -1))
-         (rotated-grid (create-empty-grid new-size)))
-    (dotimes (i rows)
-      (dotimes (j cols)
-        (let ((new-x (+ i j))
-              (new-y (- (+ cols i) j)))
-          ;; Ensure the indices are within bounds
-          (when (and (>= new-x 0) (< new-x new-size)
-                     (>= new-y 0) (< new-y new-size))
-            (place-in-rotated-grid rotated-grid new-x new-y (lookup-char i j grid))))))
-    (flatten (concatenate-strings2 (mapcar (lambda (row) (remove nil row)) (2d-array-to-list rotated-grid))))))
-
-(defun rotate-grid-45-reverse (grid)
-  (let* ((rows (length grid))
-         (cols (length (first grid)))
-         (new-size (+ rows cols -1))
-         (rotated-grid (create-empty-grid new-size)))
-    (dotimes (i rows)
-      (dotimes (j cols)
-(let ((new-x (+ j (- rows i 1)))
-              (new-y (+ i j)))
-          ;; Ensure the indices are within bounds
-          (when (and (>= new-x 0) (< new-x new-size)
-                     (>= new-y 0) (< new-y new-size))
-            (place-in-rotated-grid rotated-grid new-x new-y (lookup-char i j grid))))))
-    (flatten (concatenate-strings2 (mapcar (lambda (row) (remove nil row)) (2d-array-to-list rotated-grid))))))
-
-(defun reverse-string (str)
-  (let ((char-list (coerce str 'list)))
-    (coerce (reverse char-list) 'string)))
+(defun concatenate-strings (string-list)
+  (apply #'concatenate 'string string-list))
 
 (defun reverse-strings (strings)
     (mapcar (lambda (s0) (reverse s0)) strings))
+
+(defun count-occurrences-in-list (strings substring)
+  (reduce #'+ (mapcar (lambda (str) (count-substring-occurrences str substring)) strings)))
 
 (defun count-substring-occurrences (string substring)
   (let ((count 0)
@@ -105,21 +80,13 @@
                (setf start (+ pos (length substring))))
              (return count))))))
 
-(defun count-occurrences-in-list (strings substring)
-  (reduce #'+ (mapcar (lambda (str) (count-substring-occurrences str substring)) strings)))
-
-(defun flatten (nested-list)
-  (if (atom nested-list)
-      (list nested-list)
-      (apply #'append (mapcar #'flatten nested-list))))
-
 (defparameter *file-contents* (read-file "data.txt"))
-(defparameter *lines* (split-sequence:split-sequence #\Newline *file-contents*))
+(defparameter *lines* (split-sequence #\Newline *file-contents*))
 (defparameter *grid* (mapcar #'string-to-string-list *lines*))
-(defparameter *flat-list* (copy-and-flatten-grid *grid*))
 (defparameter *rotated-grid*  (rotate-grid-45 *grid*))
 (defparameter *rotated-grid-reverse* (rotate-grid-45-reverse *grid*))
 
+(defparameter *flat-list* (copy-and-flatten-grid *grid*))
 (defparameter *joined-list* (append *flat-list* *rotated-grid* *rotated-grid-reverse*))
 (defparameter *final-strings* (append *joined-list* (reverse-strings *joined-list*)))
 
