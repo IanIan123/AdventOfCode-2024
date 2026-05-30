da3ad58542f86d3e6a74e5653ab082793e334e6e
Ian <ian.warburton3000@gmail.com>
2026-05-30 17:00:04 +0100

    Refactor

--- Day-02/part1.lisp
+++ Day-02/part1.lisp
@@ -15,27 +15,22 @@
     (mapcar #'parse-integer (split-sequence:split-sequence #\Space str)))
 
 (defparameter *items0* (mapcar #'string-to-number-list *lines*))
+
 (defparameter *items1* (mapcar (lambda (line) (list (butlast line) (rest line))) *items0*))
+
 (defparameter *items2* (mapcar (lambda (item)
     (mapcar (lambda (x y) (cons x y))
         (first item)
         (second item)))
     *items1*))
 
-(defparameter *validDiffs0* (remove-if-not
-    (lambda (pairs)
-        (every (lambda (pair)
-            (let ((diff (abs (- (first pair) (cdr pair)))))
-                (and (<= diff 3) (> diff 0))))
-        pairs))
-    *items2*))
+(defun safe-report-p (pairs)
+  (let* ((diffs (mapcar (lambda (pair) (- (first pair) (cdr pair))) pairs))
+      (abs-ok (every (lambda (d) (and (<= (abs d) 3) (/= d 0))) diffs)))
+    (and abs-ok
+      (or (every (lambda (d) (< d 0)) diffs)
+       (every (lambda (d) (> d 0)) diffs)))))
 
-(defparameter *validDiffs1* (remove-if-not
-    (lambda (pairs)
-        (let ((diffs (mapcar (lambda (pair) (- (first pair) (cdr pair))) pairs)))
-            (or
-                (every (lambda (diff) (< diff 0)) diffs)
-                (every (lambda (diff) (> diff 0)) diffs))))
-    *validDiffs0*))
+(defparameter *validDiffs* (remove-if-not #'safe-report-p *items2*))
 
-(format t "~A~%" (length *validDiffs1*)))
+(format t "~A~%" (length *validDiffs*))
